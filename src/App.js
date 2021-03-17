37221e7e61230f3c050e758b9462ee0d02905821
Marcelo Fernando Alves Nogueira <mafealno@gmail.com>
2021-03-17 15:36:20 -0300

    update App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import { Redirect, Route } from "react-router-dom";
+import Rota from "./componentes/Rota/Rota";
+import OpcaoMenu from "./componentes/OpcaoMenu/OpcaoMenu";
+import MenuAplicativo from "./componentes/MenuAplicativo/MenuAplicativo";
+import ItemMenuPrincipal from "./componentes/ItemMenuPrincipal/ItemMenuPrincipal";
 import './App.css';
+import './EstiloGlobal.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Redirect to={{ pathname:"/dashboard" }} />
+      <div id="container-app">
+        <header id="espaco-cabecalho" className="p-10-px">
+          <OpcaoMenu>
+            <MenuAplicativo />
+          </OpcaoMenu>
+        </header>
+        <main id="espaco-conteudo">
+          <div id="espaco-esquerdo" className="p-10-px"></div>
+          <div id="espaco-centro" className="p-10-px">
+            <Rota 
+            caminho="/dashboard" 
+            exigeAutenticacao={false}
+            componente={()=> 
+            <>
+              <ItemMenuPrincipal titulo="Gestão de usuários" acaoExecutar={"/usuarios"}>
+                Criação de usuários do sistema
+              </ItemMenuPrincipal>
+              <ItemMenuPrincipal titulo="Gestão de Campanhas" acaoExecutar={"/campanhas"}>
+                Gerar arquivos de retorno e zerar campanhas
+              </ItemMenuPrincipal>
+              <ItemMenuPrincipal titulo="Historico de Arquivos de Retorno" acaoExecutar={"/arquivo-retorno"}>
+                Historico de geração de arquivo de retorno
+              </ItemMenuPrincipal>
+              <ItemMenuPrincipal titulo="Historico de Limpeza" acaoExecutar={"/limpeza"}>
+                Historico de limpeza de Campanhas
+              </ItemMenuPrincipal>
+              <ItemMenuPrincipal titulo="Saneamento de mailing" acaoExecutar={"/saneamento"}>
+                Regras de Saneamento do processo de importação das campanhas
+              </ItemMenuPrincipal>
+            </>
+            }   
+            />
+            <section id="container-telas-renderizar">
+              <Route path="/usuarios" render={() => <div>usuarios</div>} />  
+              <Route path="/campanhas" render={() => <div>campanhas</div>} />  
+              <Route path="/arquivo-retorno" render={() => <div>arquivo-retorno</div>} />  
+              <Route path="/limpeza" render={() => <div>limpeza</div>} />  
+              <Route path="/saneamento" render={() => <div>saneamento</div>} />  
+            </section>    
+          </div>
+          <div id="espaco-direita" className="p-10-px"></div>
+        </main>
+        <footer id="espaco-rodape" className="p-10-px">
+          
+        </footer>
+        
+      </div>
+    </>
   );
 }
 
