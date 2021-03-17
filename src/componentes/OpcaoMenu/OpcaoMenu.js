7f2f2556fd275257b155a67d8c9dd373cef20e94
Marcelo Fernando Alves Nogueira <mafealno@gmail.com>
2021-03-17 15:26:51 -0300

    add component OpcaoMenu

---
+++ src/componentes/OpcaoMenu/OpcaoMenu.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./OpcaoMenu.css";
+
+function OpcaoMenu(props) {
+  return (
+      <p className="item-menu">
+        {props.children}
+      </p>
+  );
+}
+
+export default OpcaoMenu
---
+++ src/componentes/OpcaoMenu/index.js
@@ -0,0 +1,2 @@
+import OpcaoMenu from "./OpcaoMenu";
+export default OpcaoMenu;
