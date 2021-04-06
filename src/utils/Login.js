406f37e8e92dcdb7d2a034767dab3da9ab1cbabf
Marcelo Nogueira <mafealno@gmail.com>
2021-04-06 10:07:08 -0300

    modify Login

--- src/utils/Login.js
+++ src/utils/Login.js
@@ -1,17 +1,53 @@
 /* eslint-disable eqeqeq */
-import * as backEndUtils from "./BackEnd";
+import Cookies from "js-cookie"; 
 
-export const usuarioLogado = async (exigeAutenticacao) => {
+export const usuarioLogado = (exigeAutenticacao) => {
     
     if(exigeAutenticacao){
-        return await backEndUtils.chamarBackEnd("GET", "/login/logado", "").then((response)=>{
-            if(response.status == 200){
-                return true;
-            }else{
-                return false
-            }
-        })
+       const token = Cookies.get('X-JWT-Token');
+
+       if(token){
+           return true
+       }else{
+           return false
+       }
     }else{
         return true
     }
+}
+
+export const deslogar = () => {
+    Cookies.remove('X-JWT-Token');
+}
+
+export const logar = async (usuario, senha) => {
+
+    if(!usuario || !senha){
+        return {
+            status: 401,
+            message: "Usuário e/ou senha não preenchidos"
+        }
+    }
+
+    const requisicao = {
+        id: "",
+        usuario: usuario,
+        senha: senha,
+        email: "",
+        nome: "",
+        sobrenome: "",
+    }
+    
+    return await fetch("http://localhost:4001/login", {
+            method: "POST",
+            headers: { 
+                "Content-Type": "application/json",
+                },
+            body: JSON.stringify(requisicao),
+            })
+            .then((resposta) => resposta.json()
+            .then(dados =>{
+                Cookies.set("X-JWT-Token", dados.data); 
+                return dados;
+            }))
 }
