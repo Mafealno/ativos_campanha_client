dea86e25555c507670cafaf9ab99620c54ba9d35
Marcelo Nogueira <mafealno@gmail.com>
2021-04-06 10:06:47 -0300

    modify BackEnd

--- src/utils/BackEnd.js
+++ src/utils/BackEnd.js
@@ -1,38 +1,29 @@
-export const linkBackEnd = process.env.REACT_APP_API_URL;
+const linkBackEnd = process.env.REACT_APP_API_URL;
 
-export const chamarBackEnd = async (
-    metodo, 
-    caminho, 
-    corpo) => {
-        
-    let tokenAutenticacao = localStorage.getItem("tokenAutenticacao")
-
-    if(tokenAutenticacao){
-        tokenAutenticacao = atob(tokenAutenticacao)
-    }
+export const chamarBackEnd = async (metodo, caminho, corpo) => {
 
     if(corpo){
-        return await fetch(linkBackEnd + caminho, {
+        return await fetch(linkBackEnd + '/api' + caminho, {
             method: metodo,
+            credentials: 'include',
             headers: { 
                 "Content-Type": "application/json",
-                "Authorization": "Bearer " + tokenAutenticacao
                 },
             body: JSON.stringify(corpo),
             }).then((resposta) =>  {
             return resposta
         })
     }else{
-        return await fetch(linkBackEnd + caminho, {
+        return await fetch(linkBackEnd + '/api' + caminho, {
             method: metodo,
+            credentials: 'include',
             headers: { 
                 "Content-Type": "application/json",
-                "Authorization": "Bearer " + tokenAutenticacao
                 },
             }).then((resposta) =>  {
-            return resposta
-            }).catch((error)=>{
-                return error
+                return resposta
+            }).catch((erro)=>{
+                return erro
             })
         }
   };
