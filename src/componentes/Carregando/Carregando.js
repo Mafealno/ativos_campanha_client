75c587345b77f886b0589e6825c5c4b94f5f5693
Marcelo Nogueira <mafealno@gmail.com>
2021-04-06 10:00:57 -0300

    add Carregando

---
+++ src/componentes/Carregando/Carregando.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Carregando.css";
+
+function Carregando () {
+
+return(
+    <div className="container">
+    <div className="carregando"></div>
+    </div>
+)
+ 
+}
+export default Carregando
