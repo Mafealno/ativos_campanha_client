1e496a72242add4696db4be88d80ae764820ebd6
Marcelo Nogueira <mafealno@gmail.com>
2021-07-20 10:00:52 -0300

    add functions cadastrarLimites, atualizarLimite

--- src/utils/Limites.js
+++ src/utils/Limites.js
@@ -8,3 +8,25 @@ export const listarLimites = async () => {
         }
     })
 } 
+
+export const cadastrarLimites = async (obj) => {
+    return await backEndUtils.chamarBackEnd("POST", "/limites", obj).then((resposta) => {
+        if(resposta.status == 201) {
+            return resposta.json().then((dados) => dados);
+        }
+    })
+}
+
+export const atualizarLimite = async (obj) => {
+
+    const limite = {
+        descricao: obj.descricao.valor,
+        valor: obj.valor.valor
+    }
+
+    return await backEndUtils.chamarBackEnd("PUT", "/limites/" + obj.id.valor, limite).then((resposta) => {
+        if(resposta.status == 200) {
+            return resposta.json().then((dados) => dados);
+        }
+    });
+}
